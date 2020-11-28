33ec29ed799712ae2bf9be4a2a2602c7d8e3eba0
Ryuki Murakami <ryuki.kyoto@gmail.com>
2020-11-28 10:40:46 +0900

    Initial Commit

---
+++ DairySwiftUI/DairySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DairySwiftUIApp.swift
+//  DairySwiftUI
+//
+//  Created by Ryuki Murakami on 2020/11/28.
+//
+
+import SwiftUI
+
+@main
+struct DairySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
